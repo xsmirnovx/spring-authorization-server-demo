ae9e610babeba89ccb85ab389d97fe7255bce71d
Alexandr Smirnov <alexandr.smirnov@auto1.com>
2022-02-20 13:01:52 +0100

    chore: project cleanup

---
+++ react-app/client/src/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // OAuth info (copied from the FusionAuth admin panel)
+    clientID: 'shift-frontend',
+    clientSecret: '123456',
+    redirectURI: 'http://localhost:5001/oauth-callback',
+    applicationID: '85a03867-dccf-4882-adde-1a79aeec50df',
+
+    // our FusionAuth api key
+    apiKey: 'o9WngMh2AAp3zH7gvMYtML9sGG31A9xVY1bi3Oui-_Y',
+
+    // ports
+    clientPort: 3001,
+    serverPort: 5001,
+    fusionAuthPort: 9000
+};
