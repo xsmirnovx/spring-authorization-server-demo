b429a6503938c165c63e255f031f3d28937604a4
Alexandr Smirnov <alexandr.smirnov@auto1.com>
2022-03-08 14:03:17 +0100

    chore: cosmetics

--- authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/converter/ClientSettingsConverter.kt
+++ authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/converter/ClientSettingsConverter.kt
@@ -2,35 +2,28 @@ package io.github.xsmirnovx.oauth2.server.converter
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings
-import org.springframework.security.oauth2.server.authorization.config.TokenSettings
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
 @Converter
-class ClientSettingsConverter(
-    private val objectMapper: ObjectMapper
-) : AttributeConverter<ClientSettings, String> {
+class ClientSettingsConverter(private val objectMapper: ObjectMapper) :
+        AttributeConverter<ClientSettings, String> {
 
     override fun convertToDatabaseColumn(attribute: ClientSettings): String {
-        return try {
+
+        return kotlin.runCatching {
             objectMapper.writeValueAsString(attribute.settings)
-        } catch (ex: Exception) {
-            ""
         }
+        .getOrThrow()
     }
 
     override fun convertToEntityAttribute(dbData: String): ClientSettings {
-        val map: Map<String, Any>
-        return try {
-            map = objectMapper.readValue(
-                dbData,
-                object : TypeReference<Map<String, Any>>() {})
-            ClientSettings.withSettings(map).build()
 
-        } catch (ex: Exception) {
-            throw RuntimeException()
+        return kotlin.runCatching {
+            val map = objectMapper.readValue(dbData, object : TypeReference<Map<String, Any>>() {})
+            ClientSettings.withSettings(map).build()
         }
+        .getOrThrow()
     }
 }
