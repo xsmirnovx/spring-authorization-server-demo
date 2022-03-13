e4087716ab65d8ee6adef30d4d3292de357018c9
Alexandr Smirnov <alexandr.smirnov@auto1.com>
2022-03-13 13:54:32 +0100

    cleanup

--- react-app/server/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const config = require('../../config');
-
-router.get('/', (req, res) => {
-    res.redirect(`http://auth-server:${config.authServerPort}/oauth2/authorize?client_id=${config.clientID}&redirect_uri=${config.redirectURI}&response_type=code`);
-});
-
-module.exports = router;
