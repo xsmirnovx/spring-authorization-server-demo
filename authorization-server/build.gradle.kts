f19541fd68e287c313a4c4e0358fa06a53b69015
Alexander Smirnov <alexandr.smirnov@auto1.com>
2022-09-03 19:55:05 +0200

    feat: jdbc session (#8)

--- authorization-server/build.gradle.kts
+++ authorization-server/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.session:spring-session-jdbc")
 	implementation("org.springframework.security:spring-security-oauth2-authorization-server:0.2.1")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
