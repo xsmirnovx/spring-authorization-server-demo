4b85aff972a012e942bb0ac034f52a217038ef19
Alexander Smirnov <alexandr.smirnov@auto1.com>
2022-09-06 17:34:23 +0200

    feat: jpa auth consent repository (#17)

---
+++ authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/adapters/database/JpaAuthorizationConsentRepository.kt
@@ -0,0 +1,18 @@
+package io.github.xsmirnovx.oauth2.server.adapters.database
+
+import io.github.xsmirnovx.oauth2.server.adapters.database.entity.AuthorizationConsent
+import io.github.xsmirnovx.oauth2.server.adapters.database.entity.AuthorizationConsentId
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface JpaAuthorizationConsentRepository : JpaRepository<AuthorizationConsent?, AuthorizationConsentId?> {
+
+    fun findByRegisteredClientIdAndPrincipalName(
+        registeredClientId: String?, principalName: String?): Optional<AuthorizationConsent?>?
+
+    fun deleteByRegisteredClientIdAndPrincipalName(
+        registeredClientId: String?, principalName: String?)
+}
