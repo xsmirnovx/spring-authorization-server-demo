2bbabdfb90c8b01a57c488f098dc096aad607e08
Alexandr Smirnov <alexandr.smirnov@auto1.com>
2022-09-06 17:31:46 +0200

    feat: jpa auth consent repository

---
+++ authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/adapters/database/OAuth2AuthorizationConsentServiceImpl.kt
@@ -0,0 +1,36 @@
+package io.github.xsmirnovx.oauth2.server.adapters.database
+
+import io.github.xsmirnovx.oauth2.server.adapters.database.entity.AuthorizationConsent
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService
+import org.springframework.stereotype.Component
+import org.springframework.util.Assert
+
+@Component
+class OAuth2AuthorizationConsentServiceImpl(
+    val authorizationConsentRepository: JpaAuthorizationConsentRepository,
+) : OAuth2AuthorizationConsentService {
+
+    override fun save(authorizationConsent: OAuth2AuthorizationConsent) {
+        Assert.notNull(authorizationConsent, "authorizationConsent cannot be null")
+        authorizationConsentRepository.save(AuthorizationConsent.fromDomain(authorizationConsent))
+    }
+
+    override fun remove(authorizationConsent: OAuth2AuthorizationConsent) {
+        Assert.notNull(authorizationConsent, "authorizationConsent cannot be null")
+        authorizationConsentRepository.deleteByRegisteredClientIdAndPrincipalName(
+            authorizationConsent.registeredClientId, authorizationConsent.principalName
+        )
+    }
+
+    override fun findById(registeredClientId: String, principalName: String): OAuth2AuthorizationConsent? {
+        Assert.hasText(registeredClientId, "registeredClientId cannot be empty")
+        Assert.hasText(principalName, "principalName cannot be empty")
+        return authorizationConsentRepository
+            .findByRegisteredClientIdAndPrincipalName(registeredClientId, principalName)
+            ?.get()
+            ?.let {
+                AuthorizationConsent.toDomain(it)
+            }
+    }
+}
--- authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/adapters/database/entity/AuthorizationConsent.kt
+++ authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/adapters/database/entity/AuthorizationConsent.kt
@@ -1,3 +1,65 @@
 package io.github.xsmirnovx.oauth2.server.adapters.database.entity
 
-class AuthorizationConsent 
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent
+import org.springframework.util.StringUtils
+import java.io.Serializable
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "oauth2_authorization_consent")
+@IdClass(AuthorizationConsentId::class)
+data class AuthorizationConsent(
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "registered_client_id")
+    val registeredClient: RegisteredClient? = null,
+
+    @Id
+    val principalName: String? = null,
+
+    @Column(length = 1000)
+    val authorities: String? = null
+) {
+    companion object {
+
+        fun toDomain(entity: AuthorizationConsent) : OAuth2AuthorizationConsent? {
+            val builder = OAuth2AuthorizationConsent.withId(
+                entity.registeredClient?.id!!, entity.principalName!!
+            )
+            entity.authorities?.let {
+                for (authority in StringUtils.commaDelimitedListToSet(it)) {
+                    builder.authority(SimpleGrantedAuthority(authority))
+                }
+            }
+            return builder.build()
+        }
+
+        fun fromDomain(domain: OAuth2AuthorizationConsent): AuthorizationConsent {
+            return AuthorizationConsent(
+                registeredClient = RegisteredClient(id = domain.registeredClientId),
+                principalName = domain.principalName,
+                authorities = StringUtils.collectionToCommaDelimitedString(domain.authorities)
+            )
+        }
+    }
+}
+
+class AuthorizationConsentId : Serializable {
+    private val registeredClient: RegisteredClient? = null
+    private val principalName: String? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as AuthorizationConsentId
+        return registeredClient?.id == that.registeredClient?.id
+                && principalName == that.principalName
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(registeredClient?.id, principalName)
+    }
+}
