87d8ef1810443bd093f51076fde89a2964c26948
Alexandr Smirnov <alexandr.smirnov@auto1.com>
2022-09-06 21:48:06 +0200

    fix 500

--- authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/adapters/database/RegisteredClientRepositoryImpl.kt
+++ authorization-server/src/main/kotlin/io/github/xsmirnovx/oauth2/server/adapters/database/RegisteredClientRepositoryImpl.kt
@@ -1,14 +1,25 @@
 package io.github.xsmirnovx.oauth2.server.adapters.database
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.xsmirnovx.oauth2.server.adapters.database.entity.RegisteredClient as RegisteredClientEntity
 import io.github.xsmirnovx.oauth2.server.exception.RegisteredClientNotFoundException
+import org.springframework.security.jackson2.SecurityJackson2Modules.*
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationServerJackson2Module
 import org.springframework.stereotype.Component
 
 @Component
-class RegisteredClientRepositoryImpl(val registeredClientRepository: JpaRegisteredClientRepository)
-    : RegisteredClientRepository {
+class RegisteredClientRepositoryImpl(
+    val registeredClientRepository: JpaRegisteredClientRepository, objectMapper: ObjectMapper
+) : RegisteredClientRepository {
+
+    init {
+        val classLoader = JpaRegisteredClientRepository::class.java.classLoader
+        val securityModules = getModules(classLoader)
+        objectMapper.registerModules(securityModules)
+        objectMapper.registerModule(OAuth2AuthorizationServerJackson2Module())
+    }
 
     override fun save(registeredClient: RegisteredClient) {
         registeredClientRepository.save(RegisteredClientEntity.fromDomain(registeredClient))
